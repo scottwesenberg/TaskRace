8f2fd704aa77c599eff7219fc713609812065333
wesen <wesen@GAMEPC-SCOOT>
2023-04-13 19:27:54 -0400

    Add project files.

---
+++ TaskRace/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace threadRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read, Set Go...");
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+            //Creating and running Tasks
+            Task t1 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Speedy Gonzales";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t1Location);
+                }
+
+            });
+            Task t2 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Road Runner";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t2Location);
+
+                }
+            });
+            Task t3 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "The Flash";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t3Location);
+
+                }
+            });
+            Task t4 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "The Turtle";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t4Location);
+
+                }
+            });
+            Task t5 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "The Hare";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t5Location);
+
+                }
+            });
+
+
+
+
+            // Wait for all tasks to complete before printing "Race has ended"
+            Task.WaitAny(t5);
+
+            Console.WriteLine("Race has ended");
+        }
+        static void MoveIt(ref int location)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner");
+                return;
+            }
+        }
+
+    }
+}
